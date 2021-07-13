3fd0c7a376069b5f3c8a3eaea978887ef1de6636
windroneus1 <windroneus1@gmail.com>
2021-07-13 11:40:37 +0900

    210713_start_css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+/*
 // props.fav == {fav}
-function Food({ name, img }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I Like {name}</h2>
-      <img src={img} alt='food' />
+      <h3>{rating}/5.0</h3>
+      <img src={picture} alt='food' />
     </div>
   );
 }
+
+// 데이터 타입 체크
+// isRequired가 없으면 [해당 데이터 타입] or undefined
+// only propTypes 이름으로만 가능
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 const foodILke = [
   {
@@ -44,10 +57,109 @@ function App() {
     <div>
       <h1>hello</h1>
       {foodILke.map(dish => {
-        return <Food key={dish.id} name={dish.name} img={dish.image} />;
+        return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />;
       })}
     </div>
   );
 }
+*/
 
+/*
+// state를 사용하기 위한 class component
+// 함수 작성 시, ()를 붙이면 프로그램 실행 시 즉시 실행, 안붙이면 필요할 때 실행
+// 함수의 argument는 {}로 전달
+// constructor() --> render() --> componentDidMount()
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  state = {
+    count: 0
+  };
+
+  // current == this.state
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
+  componentDidMount() {
+    console.log('component rendering finished');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    );
+  }
+}
+*/
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  componentDidMount() {
+    //    setTimeout(()=> {
+    //      this.setState({isLoading : false})
+    //    }, 6000)
+    this.getMovies();
+  }
+
+  // async와 await는 한쌍
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
+
+    // key를 꼭 주기
+    return (
+      <section class='container'>
+        {isLoading ? (
+          <div class='loader'>
+            <span class='loader__text'>Loading...</span>
+          </div>
+        ) : (
+          <div class='movies'>
+            {movies.map(movie => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
 export default App;
